e5e69cf4c27955efb7fbd4bbdd81857c670c877e
Mapacherama <jerome_tesselaar@hotmail.com>
2021-09-29 21:10:52 +0200

    Last stuff, need to fix layout bug

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children:[
+    {path: '', component: NotesListComponent }
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
